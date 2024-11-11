f0d823e502cd4448d6268052035b9f62ec8a1817
yxoni <psy337250@gmail.com>
2024-11-11 17:58:31 +0900

    refactor: 공백 추가

--- src/main/java/store/OutputView.java
+++ src/main/java/store/OutputView.java
@@ -17,7 +17,7 @@ public class OutputView {
     }
 
     public void promotionAdditionalGuide(String productName) {
-        System.out.printf("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", productName);
+        System.out.printf("\n현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", productName);
     }
 
     public void promotionImpossibleGuide(String productName, int amount) {
